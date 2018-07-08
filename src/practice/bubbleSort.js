91971db7878f7a791e6a72fc27310033aecbc9e3
Kalalau <cantrellkalalau@gmail.com>
2018-07-08 16:37:03 -0400

    included jsdoc params notation

--- src/practice/bubbleSort.js
+++ src/practice/bubbleSort.js
@@ -1,8 +1,18 @@
+/**
+ * @param {array} array - input array with items to be swapped 
+ * @param {number} x - first index in array to swap
+ * @param {number} y -  other index in array to swap
+ */
+
 const swap = (array, x, y) => {
 	let temp = array[x];
   array[x] = array[y];
   array[y] = temp;
 };
+
+/**
+ * @param {array} arr - input array to be sorted
+ */
 
 const bubbleSortBasic = arr => {
   let outputArray = Array.from(arr);
